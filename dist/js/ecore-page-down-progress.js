018e47f848bf390945b05829cb0fc133f4b8ee36
edwardm <etmargallo@gmail.com>
2020-10-17 10:37:10 -0700

    Progress nav styling, local bootstrap, js comments for
    Addiitional styling the nav element, put in local bootstrap js, so don't depend on internet access, additional comments in the main js on the plug-in approach

--- dist/js/ecore-page-down-progress.js
+++ dist/js/ecore-page-down-progress.js
@@ -1,2 +1,24 @@
 /*! project-name v0.0.1 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 console.log('ecore-page-down-progress file loaded');
+
+// heck for dimensions and placements
+const viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+// declare vars
+// to-do, re-evaluate on window resize
+let ecoreProgressBarHeight = document.querySelector(".ecore-progress").offsetHeight;
+console.log(ecoreProgressBarHeight);
+
+// iterate through sections
+// - get count
+// - get title
+// - build nav child nodes
+
+// set nav action items
+// - on click, go to section
+// - on hover, show title
+
+// toggle tooltip
+// - on hover, show tooltip/pseudo element
+// - should show title value
--- src/js/ecore-page-down-progress.js
+++ src/js/ecore-page-down-progress.js
@@ -1,1 +1,23 @@
 console.log('ecore-page-down-progress file loaded');
+
+// heck for dimensions and placements
+const viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+// declare vars
+// to-do, re-evaluate on window resize
+let ecoreProgressBarHeight = document.querySelector(".ecore-progress").offsetHeight;
+console.log(ecoreProgressBarHeight);
+
+// iterate through sections
+// - get count
+// - get title
+// - build nav child nodes
+
+// set nav action items
+// - on click, go to section
+// - on hover, show title
+
+// toggle tooltip
+// - on hover, show tooltip/pseudo element
+// - should show title value
