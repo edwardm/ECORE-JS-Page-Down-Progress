ddb1553d68cdbdbc162723cc6ecd2a431ca83f52
edwardm <etmargallo@gmail.com>
2020-10-19 14:46:20 -0700

    README and HTML typo fixes
    README and HTML typo fixes. Continued updates to the JS for looping  through sections, indexing, data attr application and starting to build the nav anchors with createElement.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Summary
 
-This vanila JS plug-in shows a navigation element for a quick link guide to scroll to certain sections on the page. In addition, the navigation also displays a background div element that grows in dimension as the page is scrolled through in the viewport.
+This vanilla JS plug-in shows a navigation element for a quick link guide to scroll to certain sections on the page. In addition, the navigation also displays a background div element that grows in dimension as the page is scrolled through in the viewport.
 
 ## What's so special?
 
-This plug-in can be  plugin to show the navigation progress bar in either horizontal or vertical format. The naviation also automatically build itself. Meaning the user just needs to define the "sections" via the unique class selector, and the plug-in will build out the UI on it's own. And of course, the user can style the look and placement of the bar as needed.
+This plug-in can be plugin to show the navigation progress bar in either horizontal or vertical format. The navigation also automatically build itself. Meaning the user just needs to define the "sections" via the unique class selector, and the plug-in will build out the UI on it's own. And of course, the user can style the look and placement of the bar as needed.
 
 ## Getting Started
 
@@ -20,7 +20,7 @@ $ npm install
 
 ## Notes
 
-- Ability to display in either horionztal format or vertical format
+-   Ability to display in either horionztal format or vertical format
 
 ## License
 
--- src/js/ecore-page-down-progress.js
+++ src/js/ecore-page-down-progress.js
@@ -1,12 +1,16 @@
+/* jshint esversion: 6 */
+
 let viewportHeight;
 let viewportWidth;
-let ecoreSectionClass = ".ecore-section";
-let ecoreTitleClass = ".ecore-title";
 let ecoreProgressBarHeight;
 let ecoreSectionsCount;
+let ecoreSectionClass = ".ecore-section";
+let ecoreTitleClass = ".ecore-title";
+let ecoreProgressClass = ".ecore-progress";
 
 // check for dimensions and placements
 function ecoreDetections() {
+    // detect viewport dimensions
     viewportHeight = Math.max(
         document.documentElement.clientHeight || 0,
         window.innerHeight || 0
@@ -15,27 +19,56 @@ function ecoreDetections() {
         document.documentElement.clientWidth || 0,
         window.innerWidth || 0
     );
-    ecoreProgressBarHeight = document.querySelector(".ecore-progress")
-        .offsetHeight;
     console.log(
         "viewportHeight: " +
             viewportHeight +
             ", viewportWidth: " +
             viewportWidth
     );
+
+    // detect progress bar height, if vertical
+    ecoreProgressBarHeight = document.querySelector(".ecore-progress")
+        .offsetHeight;
     console.log("ecoreProgressBarHeight: " + ecoreProgressBarHeight);
+
+    // detect sections count
     ecoreSectionsCount = document.querySelectorAll(".ecore-section").length;
     console.log("ecoreSectionsCount: " + ecoreSectionsCount);
+
+    // apply section indexes
     document
         .querySelectorAll(ecoreSectionClass)
         .forEach((ecoreSection, index) => {
-            ecoreSection.classList.add("ecore-section-" + index);
+            // ecoreSection.classList.add("ecore-section-" + index);
+            ecoreSection.setAttribute(
+                "data-ecore-section",
+                "ecore-section-" + index
+            );
         });
 
     // build the nav
+    const sectionArray = document.querySelectorAll(".ecore-section");
+    for (const [i, el] of sectionArray.entries()) {
+        console.log(i, el);
+
+        let navAnchor = document
+            .querySelector(ecoreProgressClass)
+            .append(document.createElement("a"));
+        // console.log(navAnchor);
+
+        // navAnchor.setAttribute("data-target", "ecore-section-" + i);
+
+        // document
+        //     // .querySelector(
+        //     //     ecoreProgressClass + " > a:nth-child(" + (i + 1) + ")"
+        //     // )
+        //     .querySelector(ecoreProgressClass)
+        //     .setAttribute("data-target", "ecore-section-" + i);
+    }
 }
 ecoreDetections();
 
+// recalculation needed for the progress bar UI
 window.addEventListener("resize", function () {
     ecoreDetections();
 });
